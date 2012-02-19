7f15b07db127ce00c4bb0d99ef506a43d7d452af
Felix Mayerhuber <felix.mayerhuber@gmx.at>
2012-02-19 23:10:08 +0100

    [OPENENGSB-2734] fixed a failing test (reason was the unsynchronized call of a map iteration)

--- components/test/src/main/java/org/openengsb/core/test/AbstractOsgiMockServiceTest.java
+++ components/test/src/main/java/org/openengsb/core/test/AbstractOsgiMockServiceTest.java
@@ -64,7 +64,7 @@ import org.slf4j.LoggerFactory;
 /**
  * Helper methods to mock the core {@link org.openengsb.core.api.OsgiUtilsService} service responsible for working with
  * the OpenEngSB osgi registry.
- *
+ * 
  * ServiceManagement-operations are performed via the {@link BundleContext}. All these calls are handled using two maps
  * to mock a service-registry (serviceReferences, services)
  */
@@ -303,10 +303,10 @@ public abstract class AbstractOsgiMockServiceTest extends AbstractOpenEngSBTest 
 
     /**
      * creates a mock of {@link ConnectorInstanceFactory} for the given connectorType and domains.
-     *
+     * 
      * Only {@link ConnectorInstanceFactory#createNewInstance(String)} is mocked to return a {@link Connector}-mock that
      * contains the given String as id.
-     *
+     * 
      * Also the factory is registered as a service with the required properties
      */
     protected ConnectorInstanceFactory createFactoryMock(String connector,
@@ -332,7 +332,7 @@ public abstract class AbstractOsgiMockServiceTest extends AbstractOpenEngSBTest 
     /**
      * creates a DomainProvider with for the given interface and name. This creates a mock of {@link DomainProvider}
      * where all String-methods return the name again.
-     *
+     * 
      * Also the service is registered with the mocked service-registry with the given name as domain-value
      */
     protected DomainProvider createDomainProviderMock(final Class<? extends Domain> interfaze, String name) {
@@ -427,7 +427,7 @@ public abstract class AbstractOsgiMockServiceTest extends AbstractOpenEngSBTest 
         return result;
     }
 
-    public void updateServiceListeners(int eventType, final ServiceReference serviceReference,
+    public synchronized void updateServiceListeners(int eventType, final ServiceReference serviceReference,
             Dictionary<String, Object> dict) {
         for (Entry<ServiceListener, Filter> entry : listeners.entrySet()) {
             Filter filter = entry.getValue();
